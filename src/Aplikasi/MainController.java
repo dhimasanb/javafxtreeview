bd2bdbc01c06d4568889fccbb60701bfee2cdf5b
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2016-12-31 15:41:09 +0700

    inisialisasi treeview dan imageview

--- src/Aplikasi/MainController.java
+++ src/Aplikasi/MainController.java
@@ -7,7 +7,12 @@ package Aplikasi;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -16,12 +21,32 @@ import javafx.fxml.Initializable;
  */
 public class MainController implements Initializable {
 
+    @FXML
+    TreeView <String> treeView;
+    
+    // Library Gambar Icon
+//    Image icon = new Image (getClass().getResourceAsStream("/Resource/icon.png"));
+    Image icon = new Image("/Resource/icon.png", 15, 15, false, false);
+
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        TreeItem<String> root = new TreeItem<>("Root", new ImageView(icon));
+        
+        TreeItem<String> nodeA = new TreeItem<>("node A", new ImageView(icon));
+        TreeItem<String> nodeB = new TreeItem<>("node B", new ImageView(icon));
+        TreeItem<String> nodeC = new TreeItem<>("node C", new ImageView(icon));
+        root.getChildren().addAll(nodeA,nodeB,nodeC);
+        
+        TreeItem<String> nodeA1 = new TreeItem<>("node A1", new ImageView(icon));
+        TreeItem<String> nodeB1 = new TreeItem<>("node B1", new ImageView(icon));
+        TreeItem<String> nodeC1 = new TreeItem<>("node C1", new ImageView(icon));
+        nodeA.getChildren().addAll(nodeA1,nodeB1,nodeC1);
+        
+        treeView.setRoot(root);
+    }
 }
